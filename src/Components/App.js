07d73765cc43f38e7531a2130e0e78548164c668
do xuan tho <xuantho995@gmail.com>
2021-02-16 18:14:04 +0700

    redux init

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+  selectSong
+} from '../actions'
+
+const App = () => {
+  return (
+    <div>
+      This is the app
+    </div>
+  )
+}
+
+export default App
---
+++ src/reducers/index.js
@@ -0,0 +1,24 @@
+export const songReducer = () => {
+  return [
+    {
+      title: 'no scrub',
+      duration: '2:24',
+    },
+    {
+      title: '1 scrub view',
+      duration: '4:44',
+    },
+    {
+      title: 'no scrub interview',
+      duration: '3:24',
+    },
+    {
+      title: 'no scrub chat',
+      duration: '2:56',
+    },
+    {
+      title: 'no scrub chit',
+      duration: '1:24',
+    },
+  ]
+}
